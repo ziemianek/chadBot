92dffaf1873d7ad9117d12fd08d358444f3564fb
Michal Ziemianek <mziemianek3@gmail.com>
2026-03-19 12:21:42 +0100

    feat: better chat message

--- internal/twitch/handlers.go
+++ internal/twitch/handlers.go
@@ -2,6 +2,9 @@ package twitch
 
 import (
 	"encoding/json"
+	"fmt"
+	"time"
+
 	"github.com/charmbracelet/log"
 )
 
@@ -18,9 +21,27 @@ func (m *WelcomeMessage) Handle(msg []byte) error {
 func (m *NotificationMessage) Handle(ch chan string, msg []byte) error {
 	var err error
 	err = json.Unmarshal(msg, m)
-	ch <- m.Payload.Event.Message.Text
+	// TODO: should be at debug level
+	log.Infof("Parsing notification message: %v", string(msg))
+	// TODO: refactor this maybe?
+	ch <- fmt.Sprintf("[%v] - [%v]: %v",
+		parseTimestamp(m.Metadata.MsgTimestamp),
+		m.Payload.Event.ChatterUserName,
+		m.Payload.Event.Message.Text,
+	)
 	log.Infof("Sent new message: [%v] to message channel", m.Payload.Event.Message.Text)
 	return err
+}
+
+func parseTimestamp(ts string) string {
+	var t time.Time
+	var err error
+	t, err = time.Parse(time.RFC3339Nano, ts)
+	if err != nil {
+		log.Errorf("error parsing timestamp: %v\n", err)
+		return ""
+	}
+	return t.Format("15:04:05")
 }
 
 func HandleMessage(ch chan string, msg []byte) error {
